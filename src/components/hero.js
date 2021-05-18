8557cdbf3f93396435b5230408133d690c816647
Michael Kinder <michael@hostbend.net>
2021-05-18 12:18:54 -0400

    update features section

--- src/components/hero.js
+++ src/components/hero.js
@@ -32,68 +32,97 @@ const Hero = () => {
         </div>
         <div className="relative px-4 sm:px-0">
           <div className="absolute inset-0 bg-gray-100 h-1/2" />
-          <div className="relative grid mx-auto overflow-hidden bg-white divide-y rounded shadow sm:divide-y-0 sm:divide-x sm:max-w-screen-sm sm:grid-cols-3 lg:max-w-screen-md">
-            <div className="inline-block p-8 text-center">
-              <div className="flex items-center justify-center w-12 h-12 mx-auto mb-4 rounded-full bg-blue-50">
-                <svg
-                  className="w-10 h-10 text-blue-900"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="font-bold tracking-wide text-gray-800">
-                Secure, single-file based container format
-              </p>
+          <div className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+            <div className="grid gap-8 row-gap-5 lg:grid-cols-3">
+                <div className="relative p-px overflow-hidden transition duration-300 transform border rounded shadow-sm hover:scale-105 group hover:shadow-xl">
+                <div className="absolute bottom-0 left-0 w-full h-1 duration-300 origin-left transform scale-x-0 bg-blue-900 group-hover:scale-x-100" />
+                <div className="absolute bottom-0 left-0 w-1 h-full duration-300 origin-bottom transform scale-y-0 bg-blue-900 group-hover:scale-y-100" />
+                <div className="absolute top-0 left-0 w-full h-1 duration-300 origin-right transform scale-x-0 bg-blue-900 group-hover:scale-x-100" />
+                <div className="absolute bottom-0 right-0 w-1 h-full duration-300 origin-top transform scale-y-0 bg-blue-900 group-hover:scale-y-100" />
+                <div className="relative p-5 bg-white rounded-sm">
+                    <div className="flex flex-col mb-2 lg:items-center lg:flex-row">
+                    <div className="flex items-center justify-center w-10 h-10 mb-4 mr-2 rounded-full bg-blue-50 lg:mb-0">
+                        <svg
+                        className="w-8 h-8 text-blue-900"
+                        stroke="currentColor"
+                        viewBox="0 0 52 52"
+                        >
+                        <polygon
+                            strokeWidth="3"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            fill="none"
+                            points="29 13 14 29 25 29 23 39 38 23 27 23"
+                        />
+                        </svg>
+                    </div>
+                    <h6 className="font-semibold leading-5">Integration over isolation</h6>
+                    </div>
+                    <p className="text-sm text-gray-900">
+                    Easily make use of GPUs, high speed networks, parallel filesystems on a cluster or server by default.
+                    </p>
+                </div>
+                </div>
+                <div className="relative p-px overflow-hidden transition duration-300 transform border rounded shadow-sm hover:scale-105 group hover:shadow-xl">
+                <div className="absolute bottom-0 left-0 w-full h-1 duration-300 origin-left transform scale-x-0 bg-blue-900 group-hover:scale-x-100" />
+                <div className="absolute bottom-0 left-0 w-1 h-full duration-300 origin-bottom transform scale-y-0 bg-blue-900 group-hover:scale-y-100" />
+                <div className="absolute top-0 left-0 w-full h-1 duration-300 origin-right transform scale-x-0 bg-blue-900 group-hover:scale-x-100" />
+                <div className="absolute bottom-0 right-0 w-1 h-full duration-300 origin-top transform scale-y-0 bg-blue-900 group-hover:scale-y-100" />
+                <div className="relative p-5 bg-white rounded-sm">
+                    <div className="flex flex-col mb-2 lg:items-center lg:flex-row">
+                    <div className="flex items-center justify-center w-10 h-10 mb-4 mr-2 rounded-full bg-blue-50 lg:mb-0">
+                        <svg
+                        className="w-8 h-8 text-blue-900"
+                        stroke="currentColor"
+                        viewBox="0 0 52 52"
+                        >
+                        <polygon
+                            strokeWidth="3"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            fill="none"
+                            points="29 13 14 29 25 29 23 39 38 23 27 23"
+                        />
+                        </svg>
+                    </div>
+                    <h6 className="font-semibold leading-5">Mobility of compute</h6>
+                    </div>
+                    <p className="text-sm text-gray-900">
+                    The single file SIF container format is easy to transport and share.
+                    </p>
+                </div>
+                </div>
+                <div className="relative p-px overflow-hidden transition duration-300 transform border rounded shadow-sm hover:scale-105 group hover:shadow-xl">
+                <div className="absolute bottom-0 left-0 w-full h-1 duration-300 origin-left transform scale-x-0 bg-blue-900 group-hover:scale-x-100" />
+                <div className="absolute bottom-0 left-0 w-1 h-full duration-300 origin-bottom transform scale-y-0 bg-blue-900 group-hover:scale-y-100" />
+                <div className="absolute top-0 left-0 w-full h-1 duration-300 origin-right transform scale-x-0 bg-blue-900 group-hover:scale-x-100" />
+                <div className="absolute bottom-0 right-0 w-1 h-full duration-300 origin-top transform scale-y-0 bg-blue-900 group-hover:scale-y-100" />
+                <div className="relative p-5 bg-white rounded-sm">
+                    <div className="flex flex-col mb-2 lg:items-center lg:flex-row">
+                    <div className="flex items-center justify-center w-10 h-10 mb-4 mr-2 rounded-full bg-blue-50 lg:mb-0">
+                        <svg
+                        className="w-8 h-8 text-blue-900"
+                        stroke="currentColor"
+                        viewBox="0 0 52 52"
+                        >
+                        <polygon
+                            strokeWidth="3"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            fill="none"
+                            points="29 13 14 29 25 29 23 39 38 23 27 23"
+                        />
+                        </svg>
+                    </div>
+                    <h6 className="font-semibold leading-5">Simple, effective security model</h6>
+                    </div>
+                    <p className="text-sm text-gray-900">
+                    You are the same user inside a container as outside, and cannot gain additional privilege by default.
+                    </p>
+                </div>
+                </div>
             </div>
-            <div className="inline-block p-8 text-center">
-              <div className="flex items-center justify-center w-12 h-12 mx-auto mb-4 rounded-full bg-blue-50">
-                <svg
-                  className="w-10 h-10 text-blue-900"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="font-bold tracking-wide text-gray-800">
-                Support for data-intensive workloads
-              </p>
-            </div>
-            <div className="inline-block p-8 text-center">
-              <div className="flex items-center justify-center w-12 h-12 mx-auto mb-4 rounded-full bg-blue-50">
-                <svg
-                  className="w-10 h-10 text-blue-900"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="font-bold tracking-wide text-gray-800">
-                Enterprise-grade features
-              </p>
-            </div>
-          </div>
+        </div>
         </div>
       </div>
     );
