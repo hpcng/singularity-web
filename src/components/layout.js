1af2f4fae277ef8182db6b6f0adf5e84042e0032
Michael Kinder <michael@hostbend.net>
2021-05-18 12:44:59 -0400

    Revert "add footer"
    This reverts commit 1ce20f15502d0ed341f281b3ce733e45bc9d50c9.

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import Footer from "./footer"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -27,7 +26,6 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
         <main>{children}</main>
-      <Footer />
     </>
   )
 }
