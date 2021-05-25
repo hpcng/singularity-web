8b48eb9d7545b6ab5db09fe189c41a0e53868cfc
Michael Kinder <michael@hostbend.net>
2021-05-24 20:25:50 -0400

    change favicon and theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,10 +23,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1D315F`,
+        theme_color: `#1D315F`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
