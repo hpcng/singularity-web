ff2ccf0021b6b221298940bdd249026b04cbe74d
Michael Kinder <michael@hostbend.net>
2021-05-28 00:33:58 -0400

    add support in config for posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,13 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
